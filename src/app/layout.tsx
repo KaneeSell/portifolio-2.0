db1b3302d4ed04f821d8a41c0116ebbf594d00f5
Daniel Henrique Sell <kanee.sell@gmail.com>
2025-07-13 18:21:24 -0300

    feat: Vercel SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./assets/styles/globals.css";
 import "devicon/devicon.min.css";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-y-hidden`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
